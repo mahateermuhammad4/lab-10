e83a4c2bf563a602325794fd1f90918c890c66d4
mahateermuhammad4 <233539@students.au.edu.pk>
2024-12-11 14:07:34 +0500

    Add files via upload

---
+++ 233503-LAB10-activity2/activity2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox1_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = "Checkbox 1 is checked.";
+        }
+
+        private void CheckBox1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = ""; 
+        }
+
+        private void CheckBox2_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = "Checkbox 2 is checked.";
+        }
+
+        private void CheckBox2_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = ""; 
+        }
+    }
+}
